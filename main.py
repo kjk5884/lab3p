0ed05dc338a56dcfedeadbf3c50622eee02ce567
Kyle Kroboth <kjk5884@psu.edu>
2020-09-15 15:35:53 +0000

    added pass instead of return(0)

--- main.py
+++ main.py
@@ -6,14 +6,14 @@
 # Breakout: 1
 
 def sum_n(n):
-  if n==0:
-    return(0)
+  if n<=0:
+    pass
   else:
     return(n + sum_n(n-1))
 
 def print_n(s,n):
   if n==0:
-    return(0)
+    pass
   else:
     print(s)
     print_n(s, n-1)
